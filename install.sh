37e6ae94d661d427d5c559fe397bb921bb04d2ff
陈韶恒 <130447001+chen-shaoheng@users.noreply.github.com>
2023-04-12 16:04:34 +0800

    Create install.sh
    Signed-off-by: 陈韶恒 <130447001+chen-shaoheng@users.noreply.github.com>

---
+++ install.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -x
+set -e
+
+
+mkdir -p $PWD/ncl-download
+cd ncl-download
+wget https://github.com/chen-shaoheng/NCL-SOURCE/releases/download/ncl-6.6.2/ncl-6.6.2.tar.gz
+tar -xvf ncl-6.6.2.tar.gz
+cd ncl-6.6.2
+
+#add lunix_aarch64 branch
+sed -i '380a case    aarch64:' config/ymake
+sed -i '381a set model   = $mach' config/ymake
+sed -i '382a set arch    = $mach' config/ymake
+sed -i '383a set sysincs = LINUX' config/ymake
+sed -i '384a set vendor  = ARM' config/ymake
+
+./Configure -V
+
+make Everything
+make all install
